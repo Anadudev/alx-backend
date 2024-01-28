f372a5a9830d934c5b00eb7b5a10fb3317ef3b21
AnaduCG <godzionwin@gmail.com>
2024-01-28 20:21:47 +0100

    update: function return type redifinition

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Simple helper function """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
      a function that takes two
      integer arguments page and page_size. The
