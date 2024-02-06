801b9a06dbbd027501d6a5ffa651dbf6720438fb
AnaduCG <godzionwin@gmail.com>
2024-02-06 12:14:55 +0000

    debugging

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Basic Babel setup """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """  """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(["LANGUAGES"])
+
+@app.route('/')
+def index():
+    return render_template('2-index.html')
+
+if __name__=="__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
